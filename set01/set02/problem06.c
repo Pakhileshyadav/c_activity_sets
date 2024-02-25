545d8a409269b8e4d4f80e111d2e824e0cf9d297
Pakhileshyadav <141308038+Pakhileshyadav@users.noreply.github.com>
2024-02-25 19:50:43 +0530

    test

--- set01/set02/problem06.c
+++ set01/set02/problem06.c
@@ -3,14 +3,10 @@
 void input_string(char *a);
 void str_reverse(char *str ,char *rev_str);
 void output( char *a, char *reverse_a);
-int main (){
+int main () {
     char str[100],rev_str[100];
     input_string(str);
     str_reverse(str,rev_str);
     output(str,rev_str);
     return 0;
-}
-void input _string (char *a) {
-    printf("Enter a string:");
-    scanf("%[^\n]s",a);
 }
